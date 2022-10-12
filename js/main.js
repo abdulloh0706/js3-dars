d253e694a2b5c958b1e51d8f7f661a74d4d93124
abdulloh0706 <114151859+abdulloh0706@users.noreply.github.com>
2022-10-12 18:38:25 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+alert('Salom')
+
+var ism = prompt('Ismingizni kiriting')
+var tel = prompt('telefon raqamingizni kiriting')
+
+console.log('telefon raqam =' , tel);
+console.log('ism =' , ism);
+
+
+var yosh = prompt('yoshingizni kiritng')
+
+if(yosh <= 16){
+    console.log('Qalesan ukam');
+}
+else if(yosh == 16){
+    console.log('Salom do`stim');
+}
+else{
+    console.log('Assalomu aleykum aka');
+}
